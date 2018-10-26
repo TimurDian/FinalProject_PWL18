4bdec7d239d5fdbf7383758e1ffb091445ae1b77
TimurDian <timur.sejati@students.amikom.ac.id>
2018-10-26 10:16:03 +0700

    sada

---
+++ array1.php
@@ -0,0 +1,12 @@
+<?php
+	$kendaraan = [
+					["mobil"=>["merk"=>"toyota","type"=>"vios"]],
+					"sepeda",
+					"truk",
+					["motor"=>["honda","yamaha","suzuki"]],
+					
+				];
+echo "<pre>";
+print_r($kendaraan);
+	
+?>
